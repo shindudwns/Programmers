02616f0651f73eb9c7a922633afd16289b225c58
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-10-09 18:58:37 +0900

    Create 소수찾기(시행착오).py
    제곱근을 생각하지 못하여 틀렸었다.

---
+++ 소수찾기(시행착오).py
@@ -0,0 +1,14 @@
+def soutsu(n) :
+    for i in range(0,len(ss)-1):
+        if n%ss[i]==0 :
+            return False    
+    return True
+def solution(n):
+    ss=[]
+    answer = 1    
+    for i in range(3,n+1) :
+        if i%2!=0 : 
+            if soutsu(i)==True :
+                answer+=1
+                ss.append(i)
+    return answer
