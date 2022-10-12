c5ac9cbdd089ea50baa8a9f5a23bd51d7ce17a4c
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-10-12 23:15:15 +0900

    Create 같은 숫자는 싫어.py

---
+++ Stack and Que/같은 숫자는 싫어.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    answer = [arr[0]]#초기값 arr 처음값
+    memory = arr[0]#전 값 초기값을 arr 처음값
+    for i in range(1,len(arr)):#초기값부터 길이까지
+        if(memory!=arr[i]):#전 값이 다르면 저장
+            answer.append(arr[i])
+            memory=arr[i]# 전 값
+    return answer
