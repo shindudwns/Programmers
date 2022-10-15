8b22207109e24b98164076a4ed23f51300b483d5
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-10-15 23:08:45 +0900

    Update 기능개발(i값 변경 안됨).py

--- Stack and Que/기능개발(i값 변경 안됨).py
+++ Stack and Que/기능개발(i값 변경 안됨).py
@@ -1,24 +1,20 @@
 def solution(progresses, speeds):
     answer = []
-    progresses_day=[]
-    for i in range(len(progresses)):
+    progresses_day=[] #걸리는 시간 기록 리스트
+    for i in range(len(progresses)):#걸리는 시간 계산 후 리스트에 넣는 과정
         if (100-progresses[i])%speeds[i]==0:
             progresses_day.append((100-progresses[i])/speeds[i])
         else :
             progresses_day.append((100-progresses[i])//speeds[i]+1)
-    check=[num * 0 for num in progresses_day]
-    check1=0
-    day=0
-    t=0
     for i in range(len(progresses_day)):
-        stack=0
-        now=i
-        for j in range(i+1,len(progresses_day)-1):
-            if progresses_day[i]>=progresses_day[j]:
-                stack+=1
-                now=j
+        stack=0 #앞에서 뒤에 같은 작업날에 낼 수 있는 지 쌓아주는 stack값
+        now=i #i값 변경을 위한 변수 선언
+        for j in range(i+1,len(progresses_day)):
+            if progresses_day[i]>=progresses_day[j]:#다음 작업물도 같이 낼 수 있을 때
+                stack+=1 
+                now=j #i값을 j값으로 건너뛰기위해 저장
             else:
                 break
         answer.append(stack+1)
-        i=now+1
+        i=now+1#i값 변경(하지만 변경이 반영이 안됨).
     return answer
