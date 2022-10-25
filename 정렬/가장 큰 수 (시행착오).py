eb0ac490b4363bee7b4f2d6501b389c83c5df46e
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-10-25 10:36:55 +0900

    Update 가장 큰 수 (시행착오).py

--- 정렬/가장 큰 수 (시행착오).py
+++ 정렬/가장 큰 수 (시행착오).py
@@ -4,11 +4,10 @@ def solution(numbers):
     aa=[]
     for i in numbers:
         aa.append(str(i))
-    print(numbers)
     aaa = itertools.permutations(aa, len(aa))
     ans = [int(''.join(i)) for i in aaa]
-    ans.sort()
-    answer=str(ans[len(ans)-1])
+    ans.sort(reverse=True)
+    answer=str(ans[0])
     return answer
   
   #순열을 이용해 풀었으나 시간초과가 남
