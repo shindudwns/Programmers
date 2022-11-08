1b69387778c17c72630baddacd9eec66b973b826
shin is god <97870136+shindudwns@users.noreply.github.com>
2022-11-08 23:48:33 +0900

    Create 위장.py

---
+++ 해시/위장.py
@@ -0,0 +1,13 @@
+def solution(clothes):
+    dic = {}
+    answer = 1
+    for i in clothes:
+        key = i[1] # 키값 value 값 구분
+        value = i[0]
+        if key in dic:#값 추가
+            dic[key].append(value) 
+        else:
+            dic[key] = [value]
+    for key in dic.keys():
+        answer = answer * (len(dic[key]) + 1) # 안입는 경우의 수를 위해 +1
+    return answer - 1
