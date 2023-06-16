7a9909eef8f6994e62b76c2135524703ca1923cf
shin is god <97870136+shindudwns@users.noreply.github.com>
2023-06-16 18:28:35 +0900

    Create 크레인 인형.py

---
+++ 크레인 인형.py
@@ -0,0 +1,14 @@
+def solution(board, moves):
+    answer= 0
+    arr=[] # 바구니 리스트
+    for i in range(len(moves)):
+        for j in range(len(board[0])):
+            if not board[j][moves[i]-1]==0:
+                if arr and arr[-1] == board[j][moves[i]-1]  :
+                    del arr[-1]
+                    answer+=2
+                else:
+                    arr.append(int(board[j][moves[i]-1]))
+                board[j][moves[i]-1]=0
+                break
+    return answer
