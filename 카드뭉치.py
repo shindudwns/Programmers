3af98f5a88ec0c14bbc53dea13ef2068f4e712e0
shin is god <97870136+shindudwns@users.noreply.github.com>
2023-08-24 22:45:28 +0900

    Update 카드뭉치.py

--- 카드뭉치.py
+++ 카드뭉치.py
@@ -4,7 +4,7 @@ def solution(cards1, cards2, goal)
     cards1=deque(cards1)
     cards2=deque(cards2)
     for i in range(len(goal))
-        if  cards1 and cards1[0]==goal[i] : #해당 값이 있는지 체크를 잘 해줘야함 que라서그럼2000플공
+        if  cards1 and cards1[0]==goal[i] : #해당 값이 있는지 체크를 잘 해줘야함 que라서그럼2000플
             cards1.popleft()    
         elif  cards2 and cards2[0]==goal[i]  :;
             cards2.popleft()
