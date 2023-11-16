8926dcd5016b218f3769b4e9856de03413c62f6f
shin is god <97870136+shindudwns@users.noreply.github.com>
2023-11-16 23:57:01 +0900

    Update 기사단원의 무기.py

--- 기사단원의 무기.py
+++ 기사단원의 무기.py
@@ -2,7 +2,7 @@ def yaksu(k):
     s=0
     for i in range(1,int(k**(1/2)+1)):
         if k%i==0:
-            s+=1# ㅇㅇ
+            s+=1# ㅇ
             if i**2 != k: #제곱이 되는 약수 중복 방지하는 과정
                     s+=1
     return s   
