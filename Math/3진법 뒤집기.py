3d130e9a806dcbfd0978ea3a71d05e680d3086dc
shin is god <97870136+shindudwns@users.noreply.github.com>
2023-12-25 23:48:52 +0900

    Update 3진법 뒤집기.py

--- Math/3진법 뒤집기.py
+++ Math/3진법 뒤집기.py
@@ -2,7 +2,7 @@ def solution(n):
     tmp,answer = ''
     while n>0:
         tmp+=str(n%3)
-        n//=3
+        n//=3;
     for i in range(len(tmp)):
         answer+=int(tmp[i])*pow(3,len(tmp)-i-1)
     return answer
