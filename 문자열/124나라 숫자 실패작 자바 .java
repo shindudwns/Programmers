1e09aa5427bbdb9e5a1c73cc34127bbdc00c2a2b
shin is god <97870136+shindudwns@users.noreply.github.com>
2024-06-08 17:50:44 +0900

    Create 124나라 숫자 실패작 자바 .java

---
+++ 문자열/124나라 숫자 실패작 자바 .java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(int n) {
+        String[] arr ={"1","2","4"};
+        String answer = "";
+        int first=n/3;
+        int last=n%3;
+        if (n==0){
+            int a=first+ "";
+            System.out.println(a);
+            // answer+=integer.tostring(first-1);
+            // answer+=arr[last];
+        }
+        // else{
+        //     answer+=integer.tostring(first);
+        //     answer+=arr[last];
+        // }
+        
+        return answer;
+    }
+}
